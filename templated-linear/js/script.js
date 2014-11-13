3557ddcb30a4f4ecf4553f90450bb66b6caa8595
Alexi <alanglade@01net.fr>
2014-11-13 10:22:34 +0100

    update

--- templated-linear/js/script.js
+++ templated-linear/js/script.js
@@ -1,10 +1,48 @@
-function initialize() {
-	var mapCanvas = document.getElementById('map_canvas');
-	var mapOptions = {
-		center: new google.maps.LatLng(44.5403, -78.5463),
-		zoom: 8,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	var map = new google.maps.Map(mapCanvas, mapOptions);
+$(document).ready(function() {
+    $( "#map_canvas_circle" ).hide();
+    initialize_marker();
+    $( "#circle" ).click(function() {
+        $( "#map_canvas_marker" ).hide();
+        $( "#map_canvas_circle" ).fadeIn("slow");
+        initialize_circles();
+    });
+});
+
+function initialize_marker() {
+    var myLatlng = new google.maps.LatLng(48.5976105,2.4929845);
+    var mapOptions = {
+        zoom: 9,
+        center: myLatlng
+    }
+    var map = new google.maps.Map(document.getElementById('map_canvas_marker'), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+    });
+
+    google.maps.event.addDomListener(window, 'load', initialize_marker);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize_circles() {
+    var kmRadius = 25;
+    var position = new google.maps.LatLng(48.5976105,2.4929845);
+    var mapOptions = {
+        zoom: 9,
+        center: position
+    }
+    var map = new google.maps.Map(document.getElementById('map_canvas_circle'), mapOptions);
+
+    var marker = new google.maps.Circle({
+        center: position,
+        map: map,
+        strokeColor: '#000',
+        strokeWeight: 2,
+        strokeOpacity: 0.5,
+        fillColor: '#f0f0f0',
+        fillOpacity: 0.5,
+        radius: kmRadius * 1000
+    });
+
+    google.maps.event.addDomListener(window, 'load', initialize_circles);
+}
